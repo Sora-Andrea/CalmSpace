f2986f3aeaa69d0114aa5b1b50dc1cd19bc277b4
Softt2 <CBCruz@student.fullsail.edu>
2026-02-08 09:49:55 +0900

    Fixed buttons

--- app/src/main/java/com/calmspace/MainActivity.kt
+++ app/src/main/java/com/calmspace/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.7navigation.compose.NavHost
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.calmspace.ui.authentication.loginScreen
